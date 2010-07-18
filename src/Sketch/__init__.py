a637c1d40da2d5009223c3920d0294f302e72c76
kkris <kris.kleine@yahoo.de>
2010-07-18 13:09:00 +0200

    added save-dialog to Sketch, small improvements

--- src/Sketch/__init__.py
+++ src/Sketch/__init__.py
@@ -4,6 +4,7 @@ from cream.contrib.melange import api
 from cream.util import random_hash
 
 import os
+import gtk
 import base64
 
 @api.register('sketch')
@@ -12,13 +13,34 @@ class Sketch(api.API):
     def __init__(self):
         api.API.__init__(self)
 
-    @api.expose
-    def save_image(self, string64):
+        interface = gtk.Builder()
+        interface.add_from_file(os.path.join(self.context.working_directory, 'save.glade'))
+       
+        self.dialog = interface.get_object('dialog')
+        self.dialog.connect('delete_event', lambda *args: self.dialog.hide())
+
+        filter = gtk.FileFilter()
+        filter.set_name('.png')
+        filter.add_mime_type('image/png')
+        filter.add_pattern('*.png')
+        self.dialog.add_filter(filter)
+
+    def save_image(self, path, string64):
         string64 = string64.replace('data:image/png;base64,', '')
-        filename = 'sketch' + random_hash(100)[:5] + '.png'
-        path = os.path.join(os.path.expanduser('~'), filename)
         with open(path, 'w') as file_:
             s = base64.decodestring(string64)
             file_.write(s)
 
-    
+    @api.expose
+    def show_save_dialog(self, string64):
+        self.dialog.set_current_folder(os.path.expanduser('~'))
+        self.dialog.show_all()
+
+        if self.dialog.run() == 1:
+            path = self.dialog.get_filename()
+            if not path.endswith('.png'):
+                path += '.png'
+            self.save_image(path, string64)
+
+        self.dialog.hide()
+
