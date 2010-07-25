4bbaadc45a2450b79c92acd4ec744a44d4fb0451
kkris <kris.kleine@yahoo.de>
2010-07-25 22:16:07 +0200

    cleanup, added fancy timedelta support

--- src/Tasks/__init__.py
+++ src/Tasks/__init__.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 
-
-from cream.contrib.melange import api
-import cream.ipc
-
-import os.path
 import gtk
 import time
+import datetime
+
+import cream.ipc
+from cream.contrib.melange import api
 
 @api.register('tasks')
 class Tasks(api.API):
@@ -17,110 +16,99 @@ class Tasks(api.API):
         self.task_manager = cream.ipc.get_object('org.cream.PIM', 
                                     '/org/cream/pim/Tasks')
         builder = gtk.Builder()
-        builder.add_from_file(os.path.join(self.context.working_directory, 
-                                                'add-dialog.glade'))
+        builder.add_from_file('add-dialog.glade')
 
         self.dialog = builder.get_object('dialog')
+        self.dialog.connect('delete_event', self.dialog.hide)
         self.title = builder.get_object('title')
         self.description = builder.get_object('description').get_buffer()
-        self.tags = builder.get_object('tags')
         self.priority = builder.get_object('priority')
         self.deadline = builder.get_object('deadline')
-
-        list_store = gtk.ListStore(str)
-        list_store.append(['Low']) 
-        list_store.append(['Medium'])
-        list_store.append(['High'])
-        self.priority.set_model(list_store)
-        cell = gtk.CellRendererText()
-        self.priority.pack_start(cell)
-        self.priority.add_attribute(cell,'text',0)
-
+        self.tags = builder.get_object('tags')
         self.calendar = builder.get_object('calendar')
-        self.calendar.connect('day-selected-double-click', self.btn_cb, 1)
-        
         self.calendar_win = builder.get_object('calendar_win')
-        self.calendar_win.connect('delete_event', lambda w,e: self.calendar_win.hide())
-        btn = builder.get_object('calendar_btn')
-        btn.connect('clicked', self.show_calendar)
-
-        btn = builder.get_object('ok_cal')
-        btn.connect('clicked', self.btn_cb, 1)
-        
-        btn = builder.get_object('cancel_cal')
-        btn.connect('clicked', self.btn_cb, -3)
-
-        
-
+        self.calendar_win.connect('delete_event', self.calendar_win.hide)
+        builder.get_object('calendar_btn').connect('clicked', self.show_calendar)
 
     @api.expose
     def add_task(self):
+        '''Add a task to the database. The data is provided by the dialog.'''
+
         @api.in_main_thread
         def add_task():
             self.reset_dialog()
-            response = self.dialog.run()
-            if response == 1:
+            if self.dialog.run() == 1:
                 data = self.get_data()
-                self.dialog.hide()
-                return self.task_manager.add_task(
-                        data['title'], 
-                        data['description'], 
-                        data['tags'], 
-                        data['priority'], 
-                        data['deadline']
-                    )
-            
+                self.task_manager.add_task(data['title'], 
+                    data['description'], data['tags'], data['priority'], 
+                    data['deadline'])
             self.dialog.hide()
 
         add_task()
 
-
     @api.expose
     def edit_task(self, id):
+        '''Edit a task with the given id.'''
+
         @api.in_main_thread
         def edit_task(id):
             task = self.task_manager.get_task(int(id))
             self.set_dialog_entries(task)
-            response = self.dialog.run()
-            if response == 1:
+            if self.dialog.run() == 1:
                 data = self.get_data()
-                self.dialog.hide()
-                return self.task_manager.edit_task(
-                        int(id), 
-                        data['title'], 
-                        data['description'], 
-                        data['tags'], 
-                        data['priority'], 
-                        data['deadline']
-                    )
-            
+                self.task_manager.edit_task(int(id), data['title'], 
+                    data['description'], data['tags'], data['priority'], data['deadline'])
             self.dialog.hide()
 
         edit_task(id)
 
     @api.expose
     def set_task_status(self, id, status):
+        '''Set the tasks status.'''
+
         @api.in_main_thread
         def set_task_status(id, status):
             self.task_manager.set_task_status(id, status)
 
         set_task_status(int(id), int(status))
         
-
     @api.expose
     def list_tasks(self):
+        '''Get all tasks which are not marked as done.'''
+
         @api.in_main_thread
         def list_tasks():
             tasks = self.task_manager.list_tasks()
             tasks = filter(lambda task: task['status'] != 2, tasks)
-            return sorted(tasks, key=lambda task: task['deadline'])
+            return map(self.convert_date_to_timedelta, sorted(tasks, key=lambda task: task['deadline']))
         
         return list_tasks()
 
 
+    def convert_date_to_timedelta(self, task):
+        today = datetime.date.fromtimestamp(time.time())
+        deadline = datetime.date.fromtimestamp(task['deadline'])
+        timedelta = abs((today - deadline).days)
 
+        if today > deadline and timedelta < 8:
+            if timedelta == 1:
+                task['deadline'] = 'yesterday'
+            else:
+                task['deadline'] = '{0} days ago'.format(str(timedelta))
+        elif today < deadline and timedelta < 8:
+            if timedelta == 1:
+                task['deadline'] = 'tomorrow'
+            else:
+                task['deadline'] = '{0} days left'.format(str(timedelta))
+        elif today == deadline:
+            task['deadline'] = 'today'
+        else:
+            task['deadline'] = '{0}.{1}.{2}'.format(deadline.day, deadline.month, deadline.year)
+        return task
 
     def get_data(self):
+        '''Retrieve the data from the dialog'''
+
         data = dict()
         data['title'] = self.title.get_text()
         data['description'] = self.description.get_text(
@@ -133,6 +121,8 @@ class Tasks(api.API):
         return data
 
     def set_dialog_entries(self, task):
+        '''When editing a task, set the entries to edit them'''
+
         self.title.set_text(task['title'])
         self.description.set_text(task['description'])
         self.priority.set_active(task['priority'])
@@ -144,6 +134,8 @@ class Tasks(api.API):
         self.tags.set_text(task['category'])
 
     def reset_dialog(self):
+        '''When adding a task, clear the dialog'''
+
         self.title.set_text('')
         self.description.set_text('')
         self.priority.set_active(-1)
@@ -154,12 +146,9 @@ class Tasks(api.API):
         self.tags.set_text('')
 
     def show_calendar(self, widget, *args):
-        response = self.calendar_win.run()
-        self.calendar_win.hide()
+        '''Show the calendarwindow'''
 
-        if response == 1:
+        if self.calendar_win.run() == 1:
             date = self.calendar.get_date()
             self.deadline.set_text('{0}.{1}.{2}'.format(date[2], date[1] + 1, date[0]))
-
-    def btn_cb(self, widget, id):
-        self.calendar_win.response(id)
+        self.calendar_win.hide()
