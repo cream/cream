a5bb3cae6307151a440568d1d37c87182c5146c8
kkris <kris.kleine@yahoo.de>
2010-10-26 11:29:22 +0200

    refactored dashboard

--- src/Dashboard/__init__.py
+++ src/Dashboard/__init__.py
@@ -4,35 +4,8 @@ from urllib import unquote
 
 from cream.contrib.melange import api
 from cream.util.subprocess import Subprocess
-from cream.contrib.desktopentries import DesktopEntry
 
-from cream.util.string import crop_string
-from cream.util.dicts import ordereddict
-from util import icon_to_base64, parse_cmd
-
-CATEGORIES = {'Development': 'Development',
-              'AudioVideo': 'Multimedia',
-              'Network': 'Network',
-              'Office': 'Office',
-              'Settings': 'Settings',
-              'System': 'System',
-              'Game': 'Games',
-              'Graphics': 'Graphics',
-              'Utility': 'Utility'
-}
-class App(object):
-
-    @classmethod
-    def from_entry(cls, entry):
-        app = {}
-        base64 = icon_to_base64(entry.icon)
-        if not base64:
-            return None
-        app['name'] = crop_string(entry.name, 8, '..')
-        app['cmd'] = parse_cmd(entry.exec_)
-        app['icon'] = base64
-        app['category'] = CATEGORIES.get(entry.recommended_category, '')
-        return app
+import dashboard
 
 @api.register('dashboard')
 class Dashboard(api.API):
@@ -40,75 +13,35 @@ class Dashboard(api.API):
     def __init__(self):
         api.API.__init__(self)
 
-        self.favorites = self.load_favorites()
-        self.entries = self.get_entries()
-
-    def get_entries(self):
-        entries = ordereddict()
-        for category in sorted(CATEGORIES.values()):
-            if getattr(self.config, category, False):
-                entries[category] = []
-        for entry in DesktopEntry.get_all():
-            if not hasattr(entry, 'icon'):
-                continue
-
-            app = App.from_entry(entry)
-            if app is None or app['category'] is None:
-                continue
-
-            elif app['category'] in entries:
-                entries[app['category']].append(app)
-
-                if app['name'] in self.favorites:
-                    index = self.favorites.index(app['name'])
-                    self.favorites[index] = app
-
-        e = []
-        for category in entries.itervalues():
-            if not category:
-                continue
-            e.append(sorted(category, key=lambda app: app['name'].lower()))
-        return e
-
-    def load_favorites(self):
-        favorites = self.config.favorites
-        if favorites == 'None':
-            return []
-        return favorites.split('\n')
+        self.dashboard = dashboard.Dashboard(self.config)
 
     @api.expose
     def get_all_apps(self):
-        return self.entries
+        return self.dashboard.get_apps()
 
     @api.expose
     def get_favorites(self):
-        return self.favorites
+        return self.dashboard.get_favorites()
 
     @api.expose
     def update_entries(self):
-        self.entries = self.get_entries()
+        self.apps = self.dashboard.update()
 
     @api.expose
     def add_favorite(self, name):
         @api.in_main_thread
         def _add_favorite():
-            favorites = self.config.favorites
-            if favorites == 'None':
-                self.config.favorites = name
-            else:
-                self.config.favorites += '\n' + name
+            self.dashboard.add_favorite(name)
         _add_favorite()
 
     @api.expose
     def launch_app(self, cmd, arg):
         cmd = cmd.strip()
         cmd = str(cmd).split()
-
         if arg:
             cmd += [self.parse_arg(arg)]
 
         Subprocess(cmd, fork=True).run()
-
 
     def parse_arg(self, arg):
         arg = str(arg)
---
+++ src/Dashboard/dashboard.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+from cream.contrib.desktopentries import DesktopEntry
+
+from cream.util.string import crop_string
+from cream.util.dicts import ordereddict
+
+from util import icon_to_base64, parse_cmd
+
+
+CATEGORIES = {'Development': 'Development',
+              'AudioVideo': 'Multimedia',
+              'Network': 'Network',
+              'Office': 'Office',
+              'Settings': 'Settings',
+              'System': 'System',
+              'Game': 'Games',
+              'Graphics': 'Graphics',
+              'Utility': 'Utility'
+}
+
+def app_from_entry(entry):
+    if not hasattr(entry, 'icon'):
+        return None
+    base64 = icon_to_base64(entry.icon)
+    if not base64:
+        return None
+
+    app = {}
+    app['name'] = crop_string(entry.name, 8, '..')
+    app['cmd'] = parse_cmd(entry.exec_)
+    app['icon'] = base64
+    app['category'] = CATEGORIES.get(entry.recommended_category, '')
+    return app
+
+
+class Dashboard(object):
+
+    def __init__(self, config):
+        self.config = config
+
+        self.apps = self._parse_apps_from_entries()
+        self.favorites = self._parse_favorites_from_config()
+
+    def get_apps(self):
+        return self.apps
+
+    def get_favorites(self):
+        return self.favorites
+
+    def update(self):
+        self.apps = self._parse_apps_from_entries()
+        return self.apps
+
+    def add_favorite(name):
+        favorites = self.config.favorites
+        if not favorites:
+            self.config.favorites = name
+        else:
+            self.config.favorites += '\n' + name
+
+    def _parse_apps_from_entries(self):
+        categories = self._get_categories_from_config()
+
+        for entry in DesktopEntry.get_all():
+            app = app_from_entry(entry)
+            if app is None or app['category'] is None:
+                continue
+            elif app['category'] in categories:
+                categories[app['category']].append(app)
+
+        apps = []
+        for category in categories.itervalues():
+            if category:
+                apps.append(sorted(category, key=lambda app: app['name'].lower()))
+        return apps
+
+    def _get_categories_from_config(self):
+        categories = ordereddict()
+        for category in sorted(CATEGORIES.values()):
+            if getattr(self.config, category, False):
+                categories[category] = []
+        return categories
+
+
+    def _parse_favorites_from_config(self):
+        favorites = self.config.favorites
+        if not favorites:
+            return []
+
+        favorites = favorites.split('\n')
+        for entry in DesktopEntry.get_all():
+            if entry.name in favorites:
+                app = app_from_entry(entry)
+                index = favorites.index(entry.name)
+                favorites[index] = app
+        return favorites
