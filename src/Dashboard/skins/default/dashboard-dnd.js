0f102281c50b1b3623078294f6c481105698cfe7
kkris <kris.kleine@yahoo.de>
2010-10-30 01:08:14 +0200

    huge refactoring of dashboardwidget
    simplified code a lot and implemented full drag&drop. the order of the favorites can now be changed via dnd.

---
+++ src/Dashboard/skins/default/dashboard-dnd.js
@@ -0,0 +1,53 @@
+function makeDragable(application){
+    var drag = new Drag.Move(application, {
+        droppables: $$('.favorites', '.overlay'),
+
+        onDrop: function(element, droppable, event){
+            if(droppable && droppable.id == 'favorites'){
+                if(!favorites.contains(element) && favorites.length < 4){
+                    // add this app to favorites
+                    var app = applications[element.id];
+                    add_favorite(app);
+                    widget.api.dashboard.add_favorite(app['name']);
+                } else {
+                    // reorder favorites
+                    var offset = parseFloat(element.style.left.replace('px;'));
+                    reorder_favorites(element, offset);
+                }
+            }
+            if(droppable && droppable.id == 'remove'){
+                if(favorites.contains(element)){
+                    favorites.erase(element);
+                    element.dispose();
+                    droppable.fade('out');
+                    widget.api.dashboard.remove_favorite(element.id.replace('favorite-',''));
+                }
+            }
+
+            application.just_dragged = true;
+            application.style.left = 0;
+            application.style.top = 0;
+        },
+        onStart: function(element){
+            favorites.each(function(favorite){
+                if(favorite != element)
+                    favorite.fade(0.5);
+            });
+        },
+        onComplete: function(){
+            favorites.each(function(element){
+                element.fade(0.7);
+            });
+        },
+        onEnter: function(element, droppable){
+            if(droppable.id == 'remove' && element.id.test('favorite-'))
+                droppable.fade('in');
+        },
+        onLeave: function(element, droppable){
+            if(droppable.id == 'remove' && element.id.test('favorite-'))
+                droppable.fade('out');
+        }
+    });
+
+    return application;
+}
--- src/Dashboard/skins/default/dashboard.js
+++ src/Dashboard/skins/default/dashboard.js
@@ -1,3 +1,49 @@
+var applications = {}
+var favorites = new Array();
+
+function appToHtml(app, isFavorite){
+    var wrapper = new Element('div', {
+        'class': 'application',
+        events: {
+            click: function(){
+                if(wrapper.just_dragged)
+                    wrapper.just_dragged = false;
+                else
+                    launch_app(app['cmd'], null);
+            },
+            drop: function(data){
+                launch_app(app['cmd'], data[0]);
+            },
+            mouseenter: function(){
+                wrapper.fade(1);
+            },
+            mouseleave: function(){
+                wrapper.fade(0.7);
+            }
+        }
+    });
+    if(isFavorite)
+        wrapper.id = 'favorite-' + app['name'];
+    else
+        wrapper.id = app['name'];
+
+    var icon_wrapper = new Element('div', {'class': 'icon'});
+    var icon = new Element('img', {src: 'data:image/png;base64,' + app['icon']});
+    var label = new Element('div', {'class': 'label', html: app['label']});
+    icon_wrapper.grab(icon);
+    wrapper.grab(icon_wrapper);
+    wrapper.grab(label);
+
+    //make this element dragable
+    wrapper = makeDragable(wrapper);
+
+    //map the id to the app for later use
+    applications[wrapper.id] = app;
+
+    wrapper.fade(0.7);
+    return wrapper;
+}
+
 function fill_dashboard(){
     widget.api.dashboard.get_all_apps(function(apps){
         for(i=0; i < apps.length; i++){
@@ -6,11 +52,11 @@ function fill_dashboard(){
             $('dashboard').grab(divider);
 
             for(j=0; j < category.length; j++){
-                var app = create_app(category[j]);
-                $('dashboard').grab(app);
+                var element = appToHtml(category[j], false);
+                $('dashboard').grab(element);
             };
         };
-        cleardiv = new Element('div',{styles:'clear:both;'});
+        var cleardiv = new Element('div',{styles:'clear:both;'});
         $('dashboard').grab(cleardiv);
     });
 }
@@ -18,23 +64,63 @@ function fill_dashboard(){
 function load_favorites(){
     widget.api.dashboard.get_favorites(function(favs){
         for(i=0; i < favs.length; i++){
-            add_favorite(create_app(favs[i]));
+            var element = appToHtml(favs[i], true);
+            favorites.push(element);
         };
+        update_favorite_bar();
     });
-}
-
-function create_favorite_bar(){
-    var favorite_bar = new Element('div',{
-        'class': 'favorites',
-        id: 'favorites'
-    });
-
-    return favorite_bar;
 }
 
 function add_favorite(app){
-    $('favorites').grab(app);
-    favorite_elements.push(app);
+    var element = appToHtml(app, true);
+    favorites.push(element);
+    update_favorite_bar();
+}
+
+function reorder_favorites(element, offset){
+    var old_index = favorites.indexOf(element);
+    var new_index = old_index + parseInt(offset / 90);
+    if (new_index < 0 || new_index > 3 || old_index == new_index)
+        return;
+
+    var tmp = new Array(4);
+    favorites.each(function(favorite){
+        var app = appToHtml(applications[favorite.id], true);
+        var index = favorites.indexOf(favorite);
+        if(favorite == element)
+            tmp[new_index] = app;
+
+        else if(offset > 0){
+            if(index < old_index)
+                tmp[index] = app;
+            else if(index > new_index)
+                tmp[index] = app;
+            else
+                tmp[index - 1] = app;
+        } else {
+            if(index > old_index)
+                tmp[index] = app;
+            else if(index < new_index)
+                tmp[index] = app;
+            else
+                tmp[index + 1] = app;
+        }
+    });
+
+    favorites = tmp;
+    update_favorite_bar();
+}
+
+function update_favorite_bar(){
+    $('favorites').empty();
+    var tmp = new Array();
+    favorites.each(function(element){
+        var app = applications[element.id];
+        var element = appToHtml(app, true);
+        $('favorites').grab(element);
+        tmp.push(element);
+    });
+    favorites = tmp;
 }
 
 
@@ -42,92 +128,6 @@ function update_dashboard(){
     widget.api.dashboard.update_entries();
     $('dashboard').empty();
     fill_dashboard.delay(500);
-}
-
-function create_app(app){
-    var wrapper = new Element('div', {
-        'class': 'app',
-        id: app['name'],
-        opacity: 0.7,
-        events: {
-            click: function(){
-                if (this.just_dragged)
-                    this.just_dragged = false;
-                else
-                    launch_app(app['cmd'], null);
-            },
-            drop: function(data){
-                launch_app(app['cmd'], data[0]);
-            },
-            mouseenter: function(){
-                this.fade(1);
-            },
-            mouseleave: function(){
-                this.fade(.7);
-            }
-        }
-    });
-    wrapper.just_dragged = false;
-
-    var icon_wrapper = new Element('div', {'class': 'icon'});
-    var icon = new Element('img', {src: 'data:image/png;base64,' + app['icon']});
-    icon_wrapper.grab(icon);
-
-    var label = new Element('div', {'class': 'label', html: app['label']});
-
-    wrapper.grab(icon_wrapper);
-    wrapper.grab(label);
-
-    //make the app draggable
-    var drag = new Drag.Move(wrapper, {
-        droppables: $$('.favorites', '.overlay'),
-
-        onDrop: function(element, droppable, event){
-
-            if(droppable && droppable.id == 'favorites'){
-                app = create_app(element.app);
-                if (!favorite_elements.contains(element)){
-                    add_favorite(app);
-                    widget.api.dashboard.add_favorite(app.id);
-                }
-            }
-            if(droppable && droppable.id == 'remove'){
-                if(favorite_elements.contains(element)){
-                    favorite_elements.erase(element);
-                    element.dispose();
-                    droppable.fade('out');
-                }
-            }
-
-            wrapper.just_dragged = true;
-            wrapper.style.left = 0;
-            wrapper.style.top = 0;
-        },
-        onStart: function(element){
-            favorite_elements.each(function(favorite_el){
-                if(favorite_el != element)
-                    favorite_el.fade(0.4);
-            });
-        },
-        onComplete: function(element){
-            favorite_elements.each(function(element){
-                element.fade(0.7);
-            });
-        },
-        onEnter: function(element, droppable){
-            if(droppable.id == 'remove' && favorite_elements.contains(element)){
-                droppable.fade('in');
-            }
-        },
-        onLeave: function(element, droppable){
-            if(droppable.id == 'remove' && favorite_elements.contains(element)){
-                droppable.fade('out');
-            }
-        }
-    });
-
-    wrapper.app = app;
-    return wrapper;
 }
 
 function create_divider(category){
