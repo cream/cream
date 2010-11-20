8dc81684c46a98a725de004852795639566972c9
Kristoffer Kleine <kris.kleine@yahoo.de>
2010-11-20 20:53:47 +0100

    added baseclass for all player classes

---
+++ src/Music/players/base.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import gobject
+gobject.threads_init()
+
+import dbus
+from dbus.mainloop.glib import DBusGMainLoop
+DBusGMainLoop(set_as_default=True)
+
+import thread
+
+class BasePlayerController(gobject.GObject):
+
+    __gsignals__ = {
+        'song-changed': (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, () ),
+        'state-changed': (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, () )
+    }
+
+    def __init__(self):
+        gobject.GObject.__init__(self)
+
+        self.loop = gobject.MainLoop()
+        thread.start_new_thread(self.run, ())
+
+    def run(self):
+        self.loop.run()
+
+    def quit(self):
+        raise NotImplemented
+
+    def play_pause(self):
+        raise NotImplemented
+
+    def previous(self):
+        raise NotImplemented
+
+    def next(self):
+        raise NotImplemented
+
+    def set_rating(self, rating):
+        raise NotImplemented
+
+    @property
+    def is_playing(self):
+        raise NotImplemented
+
+    @property
+    def current_track(self):
+        raise NotImplemented
+
+    def on_song_changed(self, *args):
+        self.emit('song-changed')
+
+    def on_state_changed(self, *args):
+        self.emit('state-changed')
--- src/Music/players/rhythmbox.py
+++ src/Music/players/rhythmbox.py
@@ -1,42 +1,32 @@
 #!/usr/bin/env python
 
 import dbus
-import thread
-import gobject
-gobject.threads_init()
-
-from dbus.mainloop.glib import DBusGMainLoop
-DBusGMainLoop(set_as_default=True)
-
 BUS = dbus.SessionBus()
 
-class Rhythmbox(gobject.GObject):
+from players.base import BasePlayerController
 
-    __gsignals__ = {
-        'song-changed': (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, () ),
-        'state-changed': (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, () )
-
-    }
+class Rhythmbox(BasePlayerController):
 
     def __init__(self):
-        gobject.GObject.__init__(self)
 
         self.player = BUS.get_object('org.gnome.Rhythmbox',
-                                        '/org/gnome/Rhythmbox/Player')
+                                      '/org/gnome/Rhythmbox/Player')
         self.shell = BUS.get_object('org.gnome.Rhythmbox',
-                                        '/org/gnome/Rhythmbox/Shell')
+                                     '/org/gnome/Rhythmbox/Shell')
 
         BUS.add_signal_receiver(self.on_song_changed,
                                 dbus_interface='org.gnome.Rhythmbox.Player',
                                 signal_name='playingUriChanged')
-
         BUS.add_signal_receiver(self.on_state_changed,
                                 dbus_interface='org.gnome.Rhythmbox.Player',
                                 signal_name='playingChanged')
 
-        loop = gobject.MainLoop()
-        thread.start_new_thread(loop.run, ())
+        BasePlayerController.__init__(self)
 
+
+    def quit(self):
+        BUS.remove_signal_receiver(self.on_song_changed)
+        BUS.remove_signal_receiver(self.on_state_changed)
 
     def play_pause(self):
         if self.is_playing:
@@ -59,28 +49,18 @@ class Rhythmbox(gobject.GObject):
         return self.player.getPlaying()
 
     @property
-    def track_data(self):
+    def current_track(self):
         uri = self.player.getPlayingUri()
-        data = self.shell.getSongProperties(uri)
+        track = self.shell.getSongProperties(uri)
 
         artist, album, title, tracknumber, rating, duration = \
-            map(lambda key: data.get(key, None),
+            map(lambda key: track.get(key, None),
                 ('artist', 'album', 'title', 'track-number', 'rating', 'duration'))
 
-        return dict(artist=artist,
-                    album=album,
-                    title=title,
-                    tracknumber=tracknumber,
-                    rating=rating,
-                    duration=duration
-        )
-
-    def on_song_changed(self, uri):
-        self.emit('song-changed')
-
-    def on_state_changed(self, state):
-        self.emit('state-changed')
-
-
-if __name__ == '__main__':
-    box = Rhythmbox()
+        return {'artist': artist,
+                'album': album,
+                'title': title,
+                'tracknumber': tracknumber,
+                'rating': rating,
+                'duration': duration
+        }
