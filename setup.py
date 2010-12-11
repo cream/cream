c7211ba054c8b16d5ebd45abc067bd485ea90879
Sebastian Billaudelle <stein@cream-project.org>
2010-12-11 20:01:12 +0100

    Fixed setup script.

--- setup.py
+++ setup.py
@@ -1,9 +1,29 @@
-from cream.dist import setup
+#!/usr/bin/env python
+
+import os
+from distutils.core import setup
+
+def collect_data_files():
+
+    data_files = []
+
+    for directory, directories, files in os.walk('src'):
+        rel_dir = directory.replace('src/', '')
+        for file_ in files:
+            data_files.append((
+                    os.path.join('share/cream/org.cream.Melange/data/widgets', rel_dir),
+                    [os.path.join(directory, file_)]
+            ))
+
+    return data_files
+
+
+data_files = collect_data_files()
 
 setup(
-    name = 'Melange Widgets',
-    version = '0.0.8',
-    data_files = [
-        ('share/cream/modules/melange/widgets', ['src/*'])
-        ]
-    )
+    name = 'melange-widgets',
+    version = '0.4',
+    author = 'The Cream Project (http://cream-project.org)',
+    url = 'http://github.com/cream/melange-widgets',
+    data_files = data_files,
+)
