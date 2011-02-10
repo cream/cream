51f3ba160956beda181df513d2a2d8fe30f18572
Kristoffer Kleine <kris.kleine@yahoo.de>
2011-02-10 01:50:21 +0100

    Added NotesWidget

---
+++ src/org.cream.melange.NotesWidget/__init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import os
+
+from melange import api
+
+
+@api.register('notes')
+class Notes(api.API):
+
+    def __init__(self):
+        api.API.__init__(self)
+
+        self.notes_file = os.path.join(self.context.get_path(), 'notes.txt')
+
+    @api.expose
+    def save_notes(self, notes):
+
+        with open(self.notes_file, 'w') as file_handle:
+            file_handle.write(notes)
+
+    @api.expose
+    def get_notes(self):
+
+        if not os.path.exists(self.notes_file):
+            return ''
+
+        with open(self.notes_file, 'r') as file_handle:
+             return file_handle.read()
