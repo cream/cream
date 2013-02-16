112a30eadce27123e6ff6b073b4b9279f27e727f
Kristoffer Kleine <kris.kleine@yahoo.de>
2013-02-16 18:53:20 +0100

    Fix BatteryWidget

--- src/org.cream.melange.BatteryWidget/__init__.py
+++ src/org.cream.melange.BatteryWidget/__init__.py
@@ -2,7 +2,7 @@ from melange import api
 
 import acpi
 
-@api.register('battery')
+@api.register('org.cream.melange.BatteryWidget')
 class BatteryWidget(api.API):
 
     def __init__(self):
@@ -14,9 +14,12 @@ class BatteryWidget(api.API):
 
         battery_status = acpi.battery_status()
         battery_remaining_capacity = acpi.battery_remaining_capacity(float)
-        
+
+        if battery_remaining_capacity == 'N/A': # you most likely don't have a battery
+            battery_remaining_capacity = 0.0
+
         battery_level = 'N/A'
-        
+
         if battery_remaining_capacity <= 100.0 and battery_remaining_capacity >= 85.0:
             battery_level = 'full'
 
@@ -31,11 +34,11 @@ class BatteryWidget(api.API):
 
         elif battery_remaining_capacity < 21.0 and battery_remaining_capacity >= 0.0:
             battery_level = 'empty-critical'
-        
+
         battery_remaining_capacity = str(round(battery_remaining_capacity, 2))
 
         return {
-            'status' : battery_status, 
+            'status' : battery_status,
             'level' : battery_level,
             'remaining_capacity' : battery_remaining_capacity
         }
