c2ac55e6e97328b30448ed6b8afb1d5ca5a8ed04
Kristoffer Kleine <kris.kleine@yahoo.de>
2013-02-16 01:03:39 +0100

    Fix ExampleWidget

--- src/org.cream.melange.ExampleWidget/__init__.py
+++ src/org.cream.melange.ExampleWidget/__init__.py
@@ -3,17 +3,17 @@ from melange import api
 
 # This will register the API-class as `widget.api.example`
 # accessable from your JavaScript code.
-@api.register('example')
+@api.register('org.cream.melange.ExampleWidget')
 class Example(api.API):
 
     def __init__(self):
-           
+
         api.API.__init__(self)
- 
-    #This exposes the method which can be called in you JavaScript code as 
-    # `widget.api.example.doit(arg);`
+
+    #This exposes the method which can be called in you JavaScript code as
+    # `widget.api.doit(arg);`
     @api.expose
     def doit(self, arg):
 
-        print "Got '{0}'...".format(arg)
+        self.log("Got '{0}'...".format(arg))
         return arg.capitalize()
